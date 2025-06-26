63e93a32722954367729954cb70dab12b7eee833
Topan-Roy <roytopan734@gmail.com>
2025-06-26 12:32:31 +0600

    added register firebase

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import {RouterProvider,} from "react-router";
 import 'aos/dist/aos.css';
 import { router } from './Router/Router.jsx';
 import Aos from 'aos';
+import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
 Aos.init()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <div className="font-urbanist max-w-7xl mx-auto">
-      <RouterProvider router={router} />
+     <AuthProvider>
+       <RouterProvider router={router} />
+     </AuthProvider>
      </div>
   </StrictMode>,
 )
